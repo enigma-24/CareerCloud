1b12b905c3bd21d9670a98359a65f029a42a0816
Aniket <aniket.d2402@outlook.com>
2020-07-11 15:57:35 -0400

    BaseLogic class added

--- CareerCloud.BusinessLogicLayer/ValidationException.cs
+++ CareerCloud.BusinessLogicLayer/ValidationException.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ValidationException
+    public class ValidationException : Exception
     {
+        public int Code { get; set; }
+        public ValidationException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
     }
 }
