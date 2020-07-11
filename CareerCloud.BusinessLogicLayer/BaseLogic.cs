533d37ab08358ab8b35287c311d7681d76ecec08
Aniket <aniket.d2402@outlook.com>
2020-07-11 18:45:48 -0400

    BaseLogic Added

--- CareerCloud.BusinessLogicLayer/BaseLogic.cs
+++ CareerCloud.BusinessLogicLayer/BaseLogic.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class BaseLogic
-    {
+	public abstract class BaseLogic<TPoco>
+		where TPoco : IPoco
+	{
+		protected IDataRepository<TPoco> _repository;
+		public BaseLogic(IDataRepository<TPoco> repository)
+		{
+			_repository = repository;
+		}
 
-    }
+		protected virtual void Verify(TPoco[] pocos)
+		{
+			return;
+		}
+
+		public virtual TPoco Get(Guid id)
+		{
+			return _repository.GetSingle(c => c.Id == id);
+		}
+
+		public virtual List<TPoco> GetAll()
+		{
+			return _repository.GetAll().ToList();
+		}
+
+		public virtual void Add(TPoco[] pocos)
+		{
+			foreach (TPoco poco in pocos)
+			{
+				if (poco.Id == Guid.Empty)
+				{
+					poco.Id = Guid.NewGuid();
+				}
+			}
+
+			_repository.Add(pocos);
+		}
+
+		public virtual void Update(TPoco[] pocos)
+		{
+			_repository.Update(pocos);
+		}
+
+		public void Delete(TPoco[] pocos)
+		{
+			_repository.Remove(pocos);
+		}
+	}
 }
