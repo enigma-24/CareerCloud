925e69b7564e32a64905e1554cbf9e41b34c0614
Aniket <aniket.d2402@outlook.com>
2020-07-12 17:21:26 -0400

    Business Logic added for Application date

--- CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -9,6 +9,35 @@ namespace CareerCloud.BusinessLogicLayer
     public class ApplicantJobApplicationLogic:BaseLogic<ApplicantJobApplicationPoco>
     {
         public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository) { }
-        
+
+        public override void Add(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantJobApplicationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (ApplicantJobApplicationPoco poco in pocos)
+            {
+                if (poco.ApplicationDate > DateTime.Now)
+                {
+                    exceptions.Add(new ValidationException(110, "ApplicationDate cannot be greater than today"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
