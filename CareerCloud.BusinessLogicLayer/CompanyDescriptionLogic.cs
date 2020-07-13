2c1681659f9cb4502fd5d13ec7379ecb6f458370
Aniket <aniket.d2402@outlook.com>
2020-07-12 22:15:27 -0400

    Validation for Company Description and Name

--- CareerCloud.BusinessLogicLayer/CompanyDescriptionLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyDescriptionLogic.cs
@@ -9,5 +9,38 @@ namespace CareerCloud.BusinessLogicLayer
     public class CompanyDescriptionLogic : BaseLogic<CompanyDescriptionPoco> 
     {
         public CompanyDescriptionLogic(IDataRepository<CompanyDescriptionPoco> repository):base(repository){}
+
+        public override void Add(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (CompanyDescriptionPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.CompanyDescription))
+                    exceptions.Add(new ValidationException(107, "CompanyDescription must be greater than 2 characters"));
+                else if (poco.CompanyDescription.Length <= 2)
+                    exceptions.Add(new ValidationException(107, "CompanyDescription must be greater than 2 characters"));
+
+                if (string.IsNullOrEmpty(poco.CompanyName))
+                    exceptions.Add(new ValidationException(106, "CompanyName must be greater than 2 characters"));
+                else if (poco.CompanyName.Length <= 2)
+                    exceptions.Add(new ValidationException(106, "CompanyName must be greater than 2 characters"));
+            }
+
+            if (exceptions.Count > 0)
+                throw new AggregateException(exceptions);
+        }
     }
 }
