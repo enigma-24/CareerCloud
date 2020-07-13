d4c64234b186eccf64d634371081d2848cb4e59e
Aniket <aniket.d2402@outlook.com>
2020-07-13 01:07:55 -0400

    methods created for add, update and verify

--- CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
@@ -9,5 +9,30 @@ namespace CareerCloud.BusinessLogicLayer
     public class CompanyProfileLogic:BaseLogic<CompanyProfilePoco>
     {
         public CompanyProfileLogic(IDataRepository<CompanyProfilePoco> repository):base(repository){}
+
+        public override void Add(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (CompanyProfilePoco poco in pocos)
+            {
+                // pending
+            }
+
+            if (exceptions.Count > 0)
+                throw new AggregateException(exceptions);
+        }
     }
 }
