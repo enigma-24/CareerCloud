9cbe0fd002d2dda685b6b4f5a5ad2a66a97ae08d
Aniket <aniket.d2402@outlook.com>
2020-07-13 00:22:29 -0400

    Validation for JobName and description

--- CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
@@ -9,5 +9,38 @@ namespace CareerCloud.BusinessLogicLayer
     public class CompanyJobDescriptionLogic:BaseLogic<CompanyJobDescriptionPoco>
     {
         public CompanyJobDescriptionLogic(IDataRepository<CompanyJobDescriptionPoco> repository):base(repository){}
+
+        public override void Add(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (CompanyJobDescriptionPoco poco in pocos)
+            {
+                if (poco.JobName == null)
+                    exceptions.Add(new ValidationException(300, "JobName cannot be empty"));
+                else if(poco.JobName.Trim().Length == 0)
+                    exceptions.Add(new ValidationException(300, "JobName cannot be empty"));
+
+                if (poco.JobDescriptions == null)
+                    exceptions.Add(new ValidationException(301, "JobDescriptions cannot be empty"));
+                else if (poco.JobDescriptions.Trim().Length == 0)
+                    exceptions.Add(new ValidationException(301, "JobDescriptions cannot be empty"));
+            }
+
+            if (exceptions.Count > 0)
+                throw new AggregateException(exceptions);
+        }
     }
 }
