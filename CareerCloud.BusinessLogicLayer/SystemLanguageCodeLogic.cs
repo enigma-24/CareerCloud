58ed6c57918eeb88c5b6e4976cfaba6c1a2d51c4
Aniket <aniket.d2402@outlook.com>
2020-07-14 16:29:33 -0400

    validation added for language id, name, native name

--- CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -2,30 +2,45 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CareerCloud.BusinessLogicLayer
 {
     public class SystemLanguageCodeLogic
     {
-        public SystemLanguageCodeLogic(IDataRepository<SystemLanguageCodePoco> repository) {}
+        private IDataRepository<SystemLanguageCodePoco> _repository;
+
+        public SystemLanguageCodeLogic(IDataRepository<SystemLanguageCodePoco> repository)
+        {
+            _repository = repository;
+        }
+
+        public SystemLanguageCodePoco Get(string languageId)
+        {
+            return _repository.GetSingle(c => c.LanguageID == languageId);
+        }
+
+        public List<SystemLanguageCodePoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
 
         public void Add(SystemLanguageCodePoco[] pocos)
         {
             Verify(pocos);
-            foreach (SystemLanguageCodePoco poco in pocos)
-            {
-                //add
-            }
+            _repository.Add(pocos);
         }
 
         public void Update(SystemLanguageCodePoco[] pocos)
         {
             Verify(pocos);
-            foreach (SystemLanguageCodePoco poco in pocos)
-            {
-                //update
-            }
+            _repository.Update(pocos);
+        }
+
+        public void Delete(SystemLanguageCodePoco[] pocos)
+        {
+            _repository.Remove(pocos);
         }
 
         private void Verify(SystemLanguageCodePoco[] pocos)
@@ -34,7 +49,14 @@ namespace CareerCloud.BusinessLogicLayer
 
             foreach (SystemLanguageCodePoco poco in pocos)
             {
+                if (string.IsNullOrWhiteSpace(poco.LanguageID))
+                    exceptions.Add(new ValidationException(1000, "LanguageID Cannot be empty"));
 
+                if (string.IsNullOrWhiteSpace(poco.Name))
+                    exceptions.Add(new ValidationException(1001, "Name Cannot be empty"));
+
+                if (string.IsNullOrWhiteSpace(poco.NativeName))
+                    exceptions.Add(new ValidationException(1002, "NativeName Cannot be empty"));
             }
 
             if (exceptions.Count > 0)
