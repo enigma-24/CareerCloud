e082c012b8514236ceecb9cc01d042ac2f09d707
Aniket <aniket.d2402@outlook.com>
2020-08-31 18:14:58 -0400

    Added swashbuckle to project

--- CareerCloud.WebAPI/Startup.cs
+++ CareerCloud.WebAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace CareerCloud.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,12 @@ namespace CareerCloud.WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CareerCloud REST API");
+            });
 
             app.UseEndpoints(endpoints =>
             {
